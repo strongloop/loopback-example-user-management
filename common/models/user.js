092e03fdf6dc10f7b8f9c1ed185e87cf108d1ad4
Dan Stroot <dan.stroot@gmail.com>
2014-12-11 10:00:14 -0800

    we extended User to user

--- common/models/user.js
+++ common/models/user.js
@@ -1,10 +1,10 @@
 var config = require('../../server/config.json');
 var path = require('path');
 
-module.exports = function(User) {
+module.exports = function(user) {
   //send verification email after registration
-  User.afterRemote('create', function(context, user) {
-    console.log('> User.afterRemote triggered');
+  user.afterRemote('create', function(context, user) {
+    console.log('> user.afterRemote triggered');
 
     var options = {
       type: 'email',
@@ -35,12 +35,12 @@ module.exports = function(User) {
   });
 
   //send password reset link when requested
-  User.on('resetPasswordRequest', function(info) {
+  user.on('resetPasswordRequest', function(info) {
     var url = 'http://' + config.host + ':' + config.port + '/reset-password';
     var html = 'Click <a href="' + url + '?access_token=' + info.accessToken.id
       + '">here</a> to reset your password';
 
-    User.app.models.Email.send({
+    user.app.models.Email.send({
       to: info.email,
       from: info.email,
       subject: 'Password reset',
