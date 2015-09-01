0b32989ec77f0d656c14b5d5084a3112fdd9626b
Farid Nouri Neshat <FaridN_SOAD@yahoo.com>
2015-09-01 12:34:06 +0800

    Fix typo

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   var User = app.models.user;
 
   //login page
-  app.get('/', function(res, res) {
+  app.get('/', function(req, res) {
     var credentials = dsConfig.emailDs.transports[0].auth;
     res.render('login', {
       email: credentials.user,
