336670ebe7f4910aaf2916b4e439967d3f349f56
Agnes Lin <agneslin.lin@ibm.com>
2019-05-07 15:39:57 -0400

    chore: update copyrights years

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Copyright IBM Corp. 2014,2017. All Rights Reserved.
 // Node module: loopback-example-user-management
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
